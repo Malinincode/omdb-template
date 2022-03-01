a2c508cbd58054817676c4a3d1f948228f638fed
Malin Volle <malin.volle@gmail.com>
2022-03-01 15:17:34 +0100

    klart!

--- js/main.js
+++ js/main.js
@@ -21,31 +21,17 @@
 * http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&type=series
 *
 */
-/* let url = 'https://www.omdbapi.com/?apikey=480593d7&s=marvel&type=movie'; */
-
-/* PSEUDOKOD
-* Fyll i marvel film.
-* Url kopplat till när man fyller i input. 
-* Olika filmer visas som förslag, en lista, under tiden man skriver in film. 
-* Välj vilken typ: movie, episode, serie. Visas som lista som rullgardin. 
-* Vid ej hittat någon film - felmeddelande: Sorry can´t find the movie. Try again!
 
 
-
-*/
-
-
-
-const data = async(search) => {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=480593d7&type=movie&s=marvel+${search}`);
+const data = async() => {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=480593d7&type=movie&s=marvel`);
     const movieData = await response.json();
     console.log(movieData);
     movieOutput(movieData)
     
 } 
-data("marvel");
 
- 
+
 const movieOutput = (data) => {
 console.log(data);
 console.log(data.Search[0].Title);
@@ -70,108 +56,10 @@ for(let movie of data.Search) {
 
 }
 
-document.addEventListener(`input`, function(e){
-console.log("aöflk");
-data(e.target.value)
+document.getElementById("type").addEventListener(`click`, function(e){
+
+    data();
+    e.target.remove();
 }) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-async function fetchInput() {
-
-    try {
-        let response = await fetch('https://www.omdbapi.com/?apikey=480593d7&s=marvel&type=movie');
-        console.log(response.json());
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        
-        }
-
-        let data     = await response.text();
-        document.getElementById('text').innerHTML = data
-    } catch (error) {
-        console.log(error);
-        document.getElementById('text').innerHTML = 'Opps, got some problems with the server. Please try again later';
-    } finally {
-        console.log("The finally-block is used when something is needed to be done regardless of the outcome")
-    } 
-}
-
-fetchInput();   
- */
-
-
-
-
-
-
-
-/* let text = document.getElementById('text');
-let input = document.getElementById('input')
-
-console.log(text);
- for(let text of input) {
-    text.addEventListener('text', fetchData);
-}
-
-async function fetchData(e) {
-    e.preventDefault();
-
-    text = e.target;
-    let title = text.id;
-    title = text.innerText;
-
-    try {
-        const response = await fetch('https://www.omdbapi.com/?apikey=480593d7&s=marvel&type=movie');
-        if(!response.ok) {
-            throw new Error('Ops! I can´t find your movie, try again!');
-        }
-        const data = await response.json();
-   
-        let HTMLContent = '';
-        
-        if (subject === 'movie') {
-            for (let post of data[subject]) {
-                console.log(post);
-
-                HTMLContent += `
-                    <article>
-                        <h2>${post.Title}</h2>
-                        <i>${post.Year}</i>
-                        <p>${post.imdbID}</p>
-                        <p class="left-indent">Tags: ${post.tags.join(', ')}</p>
-                    </article>
-                `
-            }
-        } else {
-            HTMLContent = `
-                <h1>${title}</h1>
-                <p>${data[subject]}</p>
-            `
-        }
-            
-        content.innerHTML = HTMLContent;
-    } catch(error) {
-        console.log(error);
-    }
-}
-   
- */
-
-
 
 
